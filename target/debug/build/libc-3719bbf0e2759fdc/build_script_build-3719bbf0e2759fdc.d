9e47ac34d4655b61237a6af2d04c272fa507ca74
doku2684 <joonkim2684@gmail.com>
2023-12-22 00:06:49 +0900

    hecto editor

---
+++ target/debug/build/libc-3719bbf0e2759fdc/build_script_build-3719bbf0e2759fdc.d
@@ -0,0 +1,5 @@
+/Users/joon/Desktop/Coding/Rust/hecto/target/debug/build/libc-3719bbf0e2759fdc/build_script_build-3719bbf0e2759fdc: /Users/joon/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs
+
+/Users/joon/Desktop/Coding/Rust/hecto/target/debug/build/libc-3719bbf0e2759fdc/build_script_build-3719bbf0e2759fdc.d: /Users/joon/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs
+
+/Users/joon/.cargo/registry/src/index.crates.io-6f17d22bba15001f/libc-0.2.151/build.rs:
---
+++ target/debug/deps/hecto-54a14388450f52a7.d
@@ -0,0 +1,14 @@
+/Users/joon/Desktop/Coding/Rust/hecto/target/debug/deps/hecto-54a14388450f52a7.rmeta: src/main.rs src/document.rs src/editor.rs src/terminal.rs src/row.rs Cargo.toml
+
+/Users/joon/Desktop/Coding/Rust/hecto/target/debug/deps/hecto-54a14388450f52a7.d: src/main.rs src/document.rs src/editor.rs src/terminal.rs src/row.rs Cargo.toml
+
+src/main.rs:
+src/document.rs:
+src/editor.rs:
+src/terminal.rs:
+src/row.rs:
+Cargo.toml:
+
+# env-dep:CARGO_PKG_VERSION=0.1.0
+# env-dep:CLIPPY_ARGS=
+# env-dep:CLIPPY_CONF_DIR
